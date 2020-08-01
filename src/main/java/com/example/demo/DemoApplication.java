183da19387bd6b833ad26342b4ee40dfe0eaab7d
anil <adeshpanus@gmail.com>
2020-08-01 17:50:32 -0500

    Google

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloController {
+
+@GetMapping("/hello")
+	String hello(String name)
+{
+	return "hello"+name;
+}
+}
+
